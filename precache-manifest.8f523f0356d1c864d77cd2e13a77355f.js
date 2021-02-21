56057be2606bb15bbeb4664ba8007a0c1fba62a8
Michael Joseph Schneider <misc7570@colorado.edu>
2021-02-20 23:13:18 -0700

    Updated for Sensor Menu

---
+++ precache-manifest.8f523f0356d1c864d77cd2e13a77355f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c833dc49575bcec5173e1ffa75648739",
+    "url": "./index.html"
+  },
+  {
+    "revision": "38856bac03d57f1f4e01",
+    "url": "./static/css/main.52c77f81.chunk.css"
+  },
+  {
+    "revision": "fc895150c903c7cfa6fd",
+    "url": "./static/js/0.9d904658.chunk.js"
+  },
+  {
+    "revision": "c34ae197781400664133",
+    "url": "./static/js/1.2ac1159c.chunk.js"
+  },
+  {
+    "revision": "afc4316b728165fe54e6",
+    "url": "./static/js/4.0229218c.chunk.js"
+  },
+  {
+    "revision": "7ac96b31841071cf55c62b2b3001304c",
+    "url": "./static/js/4.0229218c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6687ca2f58c23d5df0a8",
+    "url": "./static/js/5.028435f1.chunk.js"
+  },
+  {
+    "revision": "e71d81781b2272f12825",
+    "url": "./static/js/6.cffa6216.chunk.js"
+  },
+  {
+    "revision": "b8775f39a179cdaa7da3",
+    "url": "./static/js/7.82ad1be7.chunk.js"
+  },
+  {
+    "revision": "0f07b9b2933e34f4f837",
+    "url": "./static/js/8.793b7a94.chunk.js"
+  },
+  {
+    "revision": "eba52bfb71976a8680b2",
+    "url": "./static/js/9.d273bc8f.chunk.js"
+  },
+  {
+    "revision": "38856bac03d57f1f4e01",
+    "url": "./static/js/main.161e3749.chunk.js"
+  },
+  {
+    "revision": "f8c5b6c055dd47ed80fb",
+    "url": "./static/js/runtime-main.c6e3172d.js"
+  }
+]);
