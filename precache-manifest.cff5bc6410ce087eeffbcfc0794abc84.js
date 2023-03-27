e2d73fd36a6e141ec72a251e17d6699a49ba1898
SchneiderCode <m.schneider.programmer@gmail.com>
2023-03-27 17:53:30 -0400

    Latest update to Website, add code view

---
+++ precache-manifest.cff5bc6410ce087eeffbcfc0794abc84.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff6231b848f87a4ec0745a7064ac000",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb038c01577916c9f228",
+    "url": "./static/css/main.5e1d113e.chunk.css"
+  },
+  {
+    "revision": "2ee7fbee4612957d3e47",
+    "url": "./static/js/0.4058de5a.chunk.js"
+  },
+  {
+    "revision": "75a1098f51340a2097ec",
+    "url": "./static/js/1.3970aafc.chunk.js"
+  },
+  {
+    "revision": "615608d5311ae563dbe8",
+    "url": "./static/js/4.31c06bfd.chunk.js"
+  },
+  {
+    "revision": "d9eaf7e8dea1b5a94369351518ab2d97",
+    "url": "./static/js/4.31c06bfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea023696e3206a6d520d",
+    "url": "./static/js/5.1c943dd2.chunk.js"
+  },
+  {
+    "revision": "b14a3cf0400e5aa0a550",
+    "url": "./static/js/6.f880f5cc.chunk.js"
+  },
+  {
+    "revision": "d723337d95c25ddef141",
+    "url": "./static/js/7.28ea9b47.chunk.js"
+  },
+  {
+    "revision": "869909f7ca8fb00bc300",
+    "url": "./static/js/8.03331546.chunk.js"
+  },
+  {
+    "revision": "e0a66ec66b54c1f7797f",
+    "url": "./static/js/9.57e3820a.chunk.js"
+  },
+  {
+    "revision": "cb038c01577916c9f228",
+    "url": "./static/js/main.12127002.chunk.js"
+  },
+  {
+    "revision": "1a16b447e408939cbba0",
+    "url": "./static/js/runtime-main.8624ed23.js"
+  },
+  {
+    "revision": "562405487e2b97b2e9c3059d54c16a16",
+    "url": "./static/media/share_project.56240548.gif"
+  }
+]);
